626d4680bcf666abd7f141debc145770520e1d9c
Carol Barrera L <cv.barrera10@uniandes.edu.co>
2017-07-23 22:32:41 -0500

    Intento de leer los datos

--- GeographicPoint.c
+++ GeographicPoint.c
@@ -2,3 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#define filas 500
+#define cols 744
+
+int main(void)
+{
+	FILE *in;
+	int i;
+	char filename[100]="map_data.txt";
+	in = fopen(filename, "r");
+	int m[filas*cols];
+	for(i=0;i<filas*cols;i++)
+	{
+		fscanf(in, "%d\n", m);
+	}
+	printf("%d\n", m[371999]);
+	fclose(in);
+}
