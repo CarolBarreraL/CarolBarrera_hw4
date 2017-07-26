1f7805b4b78ca15b226e798b47577d86145aac9e
Carol Barrera L <cv.barrera10@uniandes.edu.co>
2017-07-25 21:39:54 -0500

    creo que logre hacer funcion que genera numeros aleatorios

--- GeographicPoint.c
+++ GeographicPoint.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 #define filas 500
 #define cols 744
+
+int indice(int i, int j);
+void Alea(void);
 
 int main(void)
 {
 	FILE *in;
 	int i;
+	int oceano = 1;
+	int tierra = 0;
 	char filename[100]="map_data.txt";
 	in = fopen(filename, "r");
 	int *matriz = malloc(filas*cols*sizeof(int));
@@ -17,4 +23,30 @@ int main(void)
 		fscanf(in, "%d\n", &matriz[i]);
 	}
 	fclose(in);
+	
 }
+
+/*Youtube, programacion ATS(video 47), genera un numero aleatorio*/
+void Alea(void)
+{
+	int num;
+	srand(time(NULL));
+	num = rand()%(filas*cols + 1);
+	printf("%d\n", num);
+}
+
+
+int indice(int i, int j)
+{	
+	int num;
+	num = i*cols + j;	
+	return num;
+}
+
+
+
+
+
+
+
+
