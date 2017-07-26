6bcd7a6cebba6c29537fcc0b62795d86f2abd690
Carol Barrera L <cv.barrera10@uniandes.edu.co>
2017-07-25 20:32:08 -0500

    Comprobacion de que lei bien los datos en c

--- Plots.py
+++ Plots.py
@@ -1,2 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+datos = np.genfromtxt('datos.csv', delimiter=',')
+filas = 500
+columnas = 744
+
+
+Mapa = np.zeros((filas, columnas))
+
+for i in range(filas):
+	for j in range(columnas):
+		indice = i*columnas + j
+		Mapa[i][j]= datos[indice]
+
+plt.imshow(Mapa)
+plt.show()
