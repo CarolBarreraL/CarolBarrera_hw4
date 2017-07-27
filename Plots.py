5336e6ccabdc944cf687b1191dd603e071a9d042
carolinaog <c.ortiz84@uniandes.edu.co>
2017-07-27 09:29:42 -0500

    cambie manera de hacer circulo

--- Plots.py
+++ Plots.py
@@ -4,13 +4,17 @@ import matplotlib.pyplot as plt
 datos = np.genfromtxt('map_data.txt', delimiter=' ')
 Coord = np.genfromtxt('datos.csv', delimiter=',')
 
-x = Coord[0]
-y = Coord[1]
+y = Coord[0]
+x = Coord[1]
 r = Coord[2]
+print x,y,r
 
 fig = plt.figure()
 ax= fig.add_subplot(111)
 ax.imshow(datos)
-ax.plot(x,y, 'o', ms = r*2, mec='b', mfc='none', mew=2)
-ax.annotate('PuntoNemo', xy=(x,y), xytext =(x+100,y+50), arrowprops= dict(facecolor='black', shrinkB=r*1.2))
+ax.plot(x,y, 'o', color='r')
+circulo = plt.Circle((x,y), r, color='r', fill=False)
+ax.annotate('Punto Nemo', xy=(x,y), xytext =(146,486), arrowprops= dict(facecolor='black'))
+ax.add_artist(circulo)
+plt.show()
 plt.savefig('PuntoNemo.pdf')
