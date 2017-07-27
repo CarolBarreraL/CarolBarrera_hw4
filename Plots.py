923ef9bc454b00237ef096684857221b244537b6
Carol Barrera L <cv.barrera10@uniandes.edu.co>
2017-07-26 23:12:30 -0500

    Plotea bien, falta arreglar el c

--- Plots.py
+++ Plots.py
@@ -1,17 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-datos = np.genfromtxt('datos.csv', delimiter=',')
-filas = 500
-columnas = 744
+datos = np.genfromtxt('map_data.txt', delimiter=' ')
+Coord = np.genfromtxt('datos.csv', delimiter=',')
 
+x = Coord[0]
+y = Coord[1]
+r = Coord[2]
 
-Mapa = np.zeros((filas, columnas))
-
-for i in range(filas):
-	for j in range(columnas):
-		indice = i*columnas + j
-		Mapa[i][j]= datos[indice]
-
-plt.imshow(Mapa)
-plt.show()
+fig = plt.figure()
+ax= fig.add_subplot(111)
+ax.imshow(datos)
+ax.plot(x,y, 'o', ms = r*2, mec='b', mfc='none', mew=2)
+ax.annotate('PuntoNemo', xy=(x,y), xytext =(x+100,y+50), arrowprops= dict(facecolor='black', shrinkB=r*1.2))
+plt.savefig('PuntoNemo.pdf')
