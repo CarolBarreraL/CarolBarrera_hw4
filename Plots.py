28e238757dcb2a521362694d353bba65d24c45a7
CarolBarreraL <cv.barrera10@uniandes.edu.co>
2017-07-27 14:20:07 -0500

    FINAL

--- Plots.py
+++ Plots.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 datos = np.genfromtxt('map_data.txt', delimiter=' ')
 Coord = np.genfromtxt('datos.csv', delimiter=' ')
@@ -7,14 +8,13 @@ Coord = np.genfromtxt('datos.csv', delimiter=' ')
 y = Coord[0]
 x = Coord[1]
 r = Coord[2]
-print x,y,r
 
 fig = plt.figure()
 ax= fig.add_subplot(111)
-ax.imshow(datos)
+ax.imshow(datos, cmap = cm.winter)
 ax.plot(x,y, 'o', color='r')
 circulo = plt.Circle((x,y), r, color='r', fill=False)
-ax.annotate('Punto Nemo', xy=(x,y), xytext =(146,486), arrowprops= dict(facecolor='black'))
+ax.annotate('Punto Nemo, coord(-120,-31)', xy=(x,y), xytext =(146,486), arrowprops= dict(facecolor='black'))
 ax.add_artist(circulo)
-plt.show()
+plt.title('Punto Nemo')
 plt.savefig('PuntoNemo.pdf')
