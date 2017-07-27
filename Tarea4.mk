237be2c8feed6ae674ff0644fed4b19796b55c54
carolinaog <c.ortiz84@uniandes.edu.co>
2017-07-27 09:54:24 -0500

    Modificado, Final

--- Tarea4.mk
+++ Tarea4.mk
@@ -1,9 +1,8 @@
 PuntoNemo.pdf: datos.csv Plots.py
 	python Plots.py
 
-
 datos.csv: a.out
-	./a.out> datos.csv
+	./a.out
 
-a.out : GeographicPoint.c
+a.out: GeographicPoint.c
 	cc GeographicPoint.c -lm
